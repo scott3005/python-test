6d000c8bf02c0b062625ba73f5fa5f1613a214f7
Mustafa Kurt <kurtmustafa3005@gmail.com>
2024-10-02 11:50:27 +0300

    test

--- flask_server.py
+++ flask_server.py
@@ -3,11 +3,14 @@ from flask import Flask
 
 app=Flask(__name__)
 
-
-
 @app.route("/")
 def main():
     return "hello world" 
 
+
+
+@app.route("/live")
+def live():
+    return "live and ready"
 if __name__=="__main__":
     app.run()
